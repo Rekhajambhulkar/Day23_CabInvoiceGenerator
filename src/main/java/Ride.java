90544ab0172330ba61f2088777db054eee47876a
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-12-14 23:55:35 +0530

    Ability fo find Invoice for Multiple Rides

---
+++ src/main/java/Ride.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.invoiceservice;
+
+import org.junit.Test;
+
+public class Ride {
+    public double distance;
+    public final int time;
+
+    public Ride(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
+}
